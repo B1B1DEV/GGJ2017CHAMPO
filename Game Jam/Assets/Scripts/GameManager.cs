685c2c15a7631431e46e7406e644ef14701b6e8c
n-muller <mullernicolas91@gmail.com>
2017-01-21 13:11:11 +0100

    GameManager.cs

---
+++ Game Jam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameManager : MonoBehaviour{
+
+	public static GameManager _instance;
+	public static GameManager Instance 
+	{ 
+		get {return _instance?_instance:_instance=FindObjectOfType<GameManager>(); }
+	}
+
+	public Plateau plateau;
+	public Entite avatar;
+	public List<Firefly> fireflies;
+
+	// Use this for initialization
+	void Start () {
+		
+	} 
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
