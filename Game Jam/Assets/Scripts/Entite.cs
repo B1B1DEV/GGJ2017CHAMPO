263f957c9f58746e92632be42adbd8d5ac95de50
n-muller <mullernicolas91@gmail.com>
2017-01-21 12:24:49 +0100

    Initial commit Entite.cs

---
+++ Game Jam/Assets/Scripts/Entite.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entite : MonoBehaviour {
+
+	/// <summary>
+	/// Gets or sets the position actuelle.
+	/// SET IT ONCE PER TURN.
+	/// </summary>
+	/// <value>The position actuelle SET IT ONCE PER TURN..</value>
+	public Vector2 PositionActuelle { get; set; }
+	public bool VisibilityFrom ( Vector2 fromPos, out Dictionary<Vector2, float> intensities )
+	{
+		return false;
+	}
+}
