2894335df9ef9b37e06aa5a229164c030feb7b10
n-muller <mullernicolas91@gmail.com>
2017-01-21 02:59:15 +0100

    Constantes

---
+++ Game Jam/Assets/Scripts/Constantes.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Constantes {
+	public static int LARGEUR_PLATEAU = 10;
+	public static int HAUTEUR_PLATEAU = 10;
+}
