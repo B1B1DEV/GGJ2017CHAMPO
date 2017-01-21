a30ff8d438497b6e15dd11d6dc383078100654cb
Vincent Prat <vincent.prat@cea.fr>
2017-01-21 13:50:58 +0100

    Correction du nom des constantes de rayon

--- Game Jam/Assets/Scripts/Constantes.cs
+++ Game Jam/Assets/Scripts/Constantes.cs
@@ -6,6 +6,6 @@ public static class Constantes {
 	public static int LARGEUR_PLATEAU = 10;
 	public static int HAUTEUR_PLATEAU = 10;
     public static int N_LUCIOLES = 6;
-	public static float INNER_RADIUS = 0.5f;
-	public static float OUTER_RADIUS = Mathf.Sqrt(3)/2 * INNER_RADIUS;
+	public static float OUTER_RADIUS = 0.5f;
+	public static float INNER_RADIUS = Mathf.Sqrt(3)/2 * OUTER_RADIUS;
 }
