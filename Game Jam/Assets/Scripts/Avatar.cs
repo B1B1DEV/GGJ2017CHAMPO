1e924c94269bdfb15e118bd55a12a06447a50299
TheOneDarkling <raphael.talleux@gmail.com>
2017-01-22 14:04:02 +0100

    SOUNDS

--- Game Jam/Assets/Scripts/Avatar.cs
+++ Game Jam/Assets/Scripts/Avatar.cs
@@ -5,33 +5,27 @@ public class Avatar: Entite
 {
 	private Tile nextTile;
 	private bool nextPulse;
-	private bool openDoor;
 
 	//Sound
-	public AudioSource source;
+	public AudioSource[] sounds;
+	public AudioSource noise1;
+	public AudioSource noise2;
 
-	protected override void Start()
+	void Start()
 	{
 		base.Start ();
 		nextTile = null;
 		nextPulse = false;
-		openDoor = false;
 		Camera.main.transform.position = transform.position + new Vector3 (2, 4, 1);
 		Camera.main.transform.LookAt (transform.position);
-		Tile tile = GameManager.Instance.tiles [PositionActuelle.x, PositionActuelle.y];
-		tile.CurrentState = Tile.State.Player;
-		tile.nextState = Tile.State.Player;
-		Debug.Log (GameManager.Instance.tiles [PositionActuelle.x, PositionActuelle.y].CurrentState);
+
+		sounds = GetComponents<AudioSource> ();
+		noise1 = sounds [0];
+		noise2 = sounds [1];
 	}
 
 	public override void Move()
 	{
-		if (openDoor)
-		{
-			nextTile.type = Tile.Type.PorteOuverte;
-			nextTile.nextState = Tile.State.None;
-		}
-
 		int x = PositionActuelle.x;
 		int y = PositionActuelle.y;
 		bool bloque = true;
@@ -60,25 +54,23 @@ public class Avatar: Entite
 			GameManager.Instance.pulse.sourcePoint = transform.position + 0.5f*Vector3.up;
 			GameManager.Instance.pulse.Fireflash ();
 			//Sound
-			source.PlayOneShot(source.clip, 1.0f);
+			//source.PlayOneShot(source.clip, 1.0f);
+			noise1.Play();
 		} else if (nextTile)
 		{
-			Debug.Log (nextTile.CurrentState);
-			Debug.Log (nextTile.nextState);
 			if (nextTile.CurrentState == Tile.State.None && nextTile.nextState == Tile.State.None)
 			{
 				GameManager.Instance.tiles [x, y].nextState = Tile.State.None;
 				PositionActuelle = new Coord(nextTile.transform.position);
 				nextTile.nextState = Tile.State.Player;
 
-				// test piège et porte ouverte
+				// test piège
 				if (nextTile.type == Tile.Type.Piege)
 				{
-					GameManager.Instance.Lose ();
-				} else if (nextTile.type == Tile.Type.PorteOuverte)
-				{
-					GameManager.Instance.Win ();
+					GameManager.Instance.Lose();
 				}
+
+				noise2.Play ();
 			}
 
 			Camera.main.transform.position = transform.position + new Vector3 (2, 4, 1);
@@ -88,7 +80,6 @@ public class Avatar: Entite
 		// on remet à zéro
 		nextPulse = false;
 		nextTile = null;
-		openDoor = false;
 	}
 
 	// on récupère les commandes
@@ -98,13 +89,6 @@ public class Avatar: Entite
 			nextTile = GetMouseOveredTile ();
 			if (nextTile == null)
 				return;
-
-			if (nextTile.type == Tile.Type.Porte)
-			{
-				openDoor = true;
-				return;
-			}
-
 			Vector3 deplacement = nextTile.transform.position - PositionActuelle.ToVector3 ();
 			if (deplacement.magnitude > 2 * Constantes.INNER_RADIUS + 1e-3)
 			{
