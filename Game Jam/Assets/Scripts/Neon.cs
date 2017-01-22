8376aaf8454c550716ee306dc3bad6f76d38a1db
n-muller <mullernicolas91@gmail.com>
2017-01-22 16:49:43 +0100

    Logiozejr

---
+++ Game Jam/Assets/Scripts/Neon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Neon : MonoBehaviour {
+
+
+	public Color hlc = new Color (0.9f, 0.8f, 0.3f);
+
+	public Material mat;
+
+	private static Material _newmat = null;
+	public Material newmat { 
+		get {
+			if (_newmat == null) {
+				_newmat = new Material (mat);
+
+				_newmat.DOColor (hlc * Mathf.LinearToGammaSpace (0.3f), "_EmissionColor", 0.1f).SetLoops (-1, LoopType.Yoyo);
+				_newmat.EnableKeyword ("_EMISSION");
+			}
+			return _newmat;
+		}
+	}
+
+	public void Start()
+	{
+
+			
+		GetComponent<MeshRenderer> ().materials [1] = newmat;
+
+	}
+}
