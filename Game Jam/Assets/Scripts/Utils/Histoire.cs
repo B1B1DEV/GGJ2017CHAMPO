d14f2bfe8c0eb2843d1241e593a8aacb836f169a
n-muller <mullernicolas91@gmail.com>
2017-01-22 02:30:04 +0100

    Création d'un monstre.

---
+++ Game Jam/Assets/Scripts/Utils/Histoire.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// #lolilol
+public delegate void UpdateHistoireAction ();
+
+/// <summary>
+/// Histoire.
+/// Ceci est un monstre, mais c'est aussi mon enfant, je ne peut plus m'en débarasser maintenant. 
+/// </summary>
+public class Histoire<T> {
+	public delegate T GetValueDelegate();
+
+	private T[] histoire = new T[Constantes.MEMOIRE_ENTITEES];
+	int initialTime;
+	GetValueDelegate getter;
+
+	public Histoire ( GetValueDelegate getter )
+	{
+		initialTime = GameManager.time;
+		this.getter = getter;
+		histoire [0] = getter();
+
+		GameManager.Instance.OnUpdateHistoire += UpdateValeurActuelle;
+	}
+
+
+	~Histoire()
+	{
+		GameManager.Instance.OnUpdateHistoire -= UpdateValeurActuelle;
+	}
+	public T ValeurActuelle { get { return histoire [(GameManager.time - initialTime) % Constantes.MEMOIRE_ENTITEES]; } } 
+
+	public bool ValeurPassee(int nombreDeTour ,out T valeur)
+	{
+		if (GameManager.time - nombreDeTour - initialTime < 0) {
+			valeur = default(T);
+			return false;
+		}
+		valeur = histoire [(GameManager.time - nombreDeTour - initialTime) % Constantes.MEMOIRE_ENTITEES];
+		return true;
+	}
+		
+	public void UpdateValeurActuelle ()
+	{
+		histoire [(GameManager.time - initialTime) % Constantes.MEMOIRE_ENTITEES] = getter ();
+	}
+}
